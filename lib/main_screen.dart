a1b98f03678f156ee6721682ae32111b828441c9
Eshan Amanda <eshanesn97@gmail.com>
2022-07-01 00:30:53 +0530

    Adding initial attributes

---
+++ lib/main_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+const activeColor = Color(0xff0073dd);
+const inActiveColor = Color(0xFF212121);
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Color maleBoxColor = activeColor;
+  Color femaleBoxColor = inActiveColor;
+  int height = 180;
+  int weight = 20;
+  int age = 25;
+  String result = "";
+  String resultDetail = "Result here";
+  double bmi = 0;
+
+  void updateBoxColor(int gender) {
+    if (gender == 1) {
+      if (maleBoxColor == inActiveColor) {
+        maleBoxColor = activeColor;
+        femaleBoxColor = inActiveColor;
+      } else {
+        maleBoxColor = inActiveColor;
+        femaleBoxColor = activeColor;
+      }
+    } else {
+      if (femaleBoxColor == inActiveColor) {
+        femaleBoxColor = activeColor;
+        maleBoxColor = inActiveColor;
+      } else {
+        femaleBoxColor = inActiveColor;
+        maleBoxColor = activeColor;
+      }
+    }
+  }
+
+  String calculateBmi(int weight, int height) {
+    double bmi = weight / pow(height / 100, 2);
+    return bmi.toStringAsFixed(1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
