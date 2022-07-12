d67d625cf28dc17a51e3615c556e21df7d87dbff
Eshan Amanda <eshanesn97@gmail.com>
2022-07-12 07:39:51 +0530

    Adding weight decider part

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -57,37 +57,156 @@ class _MainScreenState extends State<MainScreen> {
         title: const Text("BMI Calculator"),
       ),
       body: Column(
-        children: [
+        children: <Widget>[
           Expanded(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  updateBoxColor(1);
-                });
-              },
-              child: ContainerBox(
-                boxColor: maleBoxColor,
-                childwidget: DataContainer(
-                    icon: FontAwesomeIcons.male, title: 'MALE'),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateBoxColor(1);
+                        });
+                      },
+                      child: ContainerBox(
+                        boxColor: maleBoxColor,
+                        childwidget: DataContainer(
+                            icon: FontAwesomeIcons.male, title: 'MALE'),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateBoxColor(2);
+                        });
+                      },
+                      child: ContainerBox(
+                        boxColor: femaleBoxColor,
+                        childwidget: DataContainer(
+                            icon: FontAwesomeIcons.female, title: 'FEMALE'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ),
+          Expanded(
+            child: ContainerBox(
+              boxColor: inActiveColor,
+              childwidget: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const Text(
+                    'HEIGHT',
+                    style: textStyle1,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(
+                        height.toString(),
+                        style: textStyle2,
+                      ),
+
+                      const Text(
+                        'cm',
+                        style: textStyle1,
+                      ),
+                    ],
+                  ),
+
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120,
+                    max: 220,
+                    activeColor: activeColor,
+                    inactiveColor: inActiveColor,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue.round();
+                      });
+                    },
+                  )
+                ],
               ),
             ),
           ),
+
           Expanded(
-            child: GestureDetector(
-              onTap: () {
-                setState(() {
-                  updateBoxColor(2);
-                });
-              },
-              child: ContainerBox(
-                boxColor: femaleBoxColor,
-                childwidget: DataContainer(
-                    icon: FontAwesomeIcons.female, title: 'FEMALE'),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ContainerBox(
+                  boxColor: inActiveColor,
+                    childwidget: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                        const Text(
+                          'WEIGHT',
+                          style: textStyle1,
+                        ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.baseline,
+                            textBaseline: TextBaseline.alphabetic,
+                              children: <Widget>[
+                                Text(
+                                  weight.toString(),
+                                  style: textStyle2,
+                                ),
+
+                                const Text(
+                                'kg',
+                                  style: textStyle1,
+                                ),
+                              ],
+                          ),
+
+                          Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                  weight++;
+                                });
+                                },
+                                  backgroundColor: activeColor,
+                                  child: const Icon(FontAwesomeIcons.plus,
+                                    color: Colors.white),
+                              ),
+
+                              const SizedBox(
+                                width: 10.0,
+                              ),
+
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    if (weight > 0) {
+                                    weight--;
+                                    }
+                                  });
+                                },
+                              backgroundColor: activeColor,
+                              child: const Icon(FontAwesomeIcons.minus,
+                                color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
   }
 }
